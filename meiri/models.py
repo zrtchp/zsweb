72c0d9eb17efa70606e4624cec4e81d48ec43e3d
zrtchp <zrtchp@qq.com>
2018-11-06 17:37:13 +0800

    new

---
+++ meiri/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class DataCs(models.Model):
+    subno = models.IntegerField(primary_key=True)
+    date = models.DateField()
+    bal = models.DecimalField(max_digits=26, decimal_places=6, blank=True, null=True)
+    bal_c = models.DecimalField(max_digits=26, decimal_places=6, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'data_cs'
+        unique_together = (('subno', 'date'),)
